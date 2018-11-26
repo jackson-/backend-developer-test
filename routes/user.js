8d5a8a3233325c8183ff4a2a3750d22328298ea8
jackson- <jackson.t.devin@gmail.com>
2018-11-26 15:37:22 -0500

    switch preferences to a property array on user with model refereces. used model.populate to get game objects. also moved the addPreference and listPlayers fucntion to users. listPlayers is now bringing back same game interest users. will implement gps coordinates later

--- routes/user.js
+++ routes/user.js
@@ -11,4 +11,25 @@ module.exports = router
             res.sendStatus(400)
         }
     })
+    .post('/listPlayers', [authService.checkTokenMW, authService.verifyToken], async (req, res) => {
+        try{
+            const {user} = req.body;
+            const players = await User.find({preferences: {$in: user.preferences}, _id: {$ne: user._id}}).populate('preferences')
+            return res.status(200).send(players)
+        } catch(e) {
+            return res.status(400).send(e.message)
+        }
+    })
+    .post('/addPreference', [authService.checkTokenMW, authService.verifyToken], async (req, res) => {
+        try{
+            await User.updateOne(
+                { _id: req.body.userId},
+                { $addToSet: {preferences: [ req.body.gameId ] } }
+            )
+            const user = await User.findOne({ _id: req.body.userId}).populate("preferences")
+            res.status(200).send(user);
+        } catch(e) {
+            res.status(400).send(e.message)
+        }
+    })
 
