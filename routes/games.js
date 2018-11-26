ac09e33625e0894d8b8f528f075c41189b88e3b9
jackson- <jackson.t.devin@gmail.com>
2018-11-26 18:14:19 -0500

    endpoints for listing players but interest and sorted by distance, create matches, list matches, send match requests, accept/deny requests, and listing requests

--- routes/games.js
+++ routes/games.js
@@ -1,6 +1,6 @@
 const router = require('express').Router();
 const authService = require('../Services/AuthService');
-const {User, Game, GamePreference} = require('../Model');
+const {User, Game, Match, Request} = require('../Model');
 
 module.exports = router
     .post('/createGame', [authService.checkTokenMW, authService.verifyToken], async (req, res) => {
@@ -8,15 +8,66 @@ module.exports = router
         if(game){
             res.sendStatus(200);
         } else {
-            res.sendStatus(301)
+            res.sendStatus(400)
         }
     })
-    .post('/addPreference', [authService.checkTokenMW, authService.verifyToken], async (req, res) => {
-        const preference = await new GamePreference({ userId:  req.body.userId, gameId: req.body.gameId}).save();
-        if(preference){
-            res.sendStatus(200);
-        } else {
-            res.sendStatus(301)
-        }
+    .post('/createMatch', [authService.checkTokenMW, authService.verifyToken], async (req, res) => {
+        try{
+            const createdMatch = await new Match({...req.body}).save();
+            const match = await Match.find({_id: createdMatch._id}).populate('players', ["name", "email", "location", "preferences", "age", "available"])
+            res.status(200).send(match)
+        }catch (e){
+            res.status(400).send(e.message)
+        }        
     })
+    .post('/listMatches', [authService.checkTokenMW, authService.verifyToken], async (req, res) => {
+        try {
+            const matches = await Match.find({open: true}).populate('players', ["name", "email", "location", "preferences", "age", "available"])
+            res.status(200).send(match)
+        } catch (e){
+            res.status(400).send(e.message)
+        }        
+    })
+    .post('/sendRequest', [authService.checkTokenMW, authService.verifyToken], async (req, res) => {
+        try {
+            const request = await new Request({...req.body}).save();
+            res.status(200).send(request)
+        } catch (e){
+            res.status(400).send(e.message)
+        }        
+    })
+    .get('/listRequests', [authService.checkTokenMW, authService.verifyToken], async (req, res) => {
+        try {
+            const requests = await Request.find()
+            res.status(200).send(requests)
+        } catch (e){
+            res.status(400).send(e.message)
+        }        
+    })
+    .post('/updateRequest', [authService.checkTokenMW, authService.verifyToken], async (req, res) => {
+        try {
+            const {answer, matchId, requestId} = req.body
+            await Request.updateOne({_id: requestId}, {answer: answer})
+            const request = await Request.findOne({_id: requestId})
+            if(answer === true){
+                const match = await Match.findOne({ _id: matchId }).populate('game')
+                if(match.game.max_players === match.players.length + 1){
+                    await Match.updateOne(
+                        { _id: matchId },
+                        { $addToSet: {players: [ request.user ] }, open: false }
+                    )
+                } else {
+                    await Match.updateOne(
+                        { _id: matchId },
+                        { $addToSet: {players: [ request.user ] } }
+                    )
+                }
+                return res.sendStatus(200)
+            }
+            return res.sendStatus(200)
+        } catch (e){
+            return res.status(400).send(e.message)
+        }       
+    })
+    
 
--- routes/index.js
+++ routes/index.js
@@ -3,5 +3,5 @@ const api = require('express').Router();
 module.exports = api
   .use('/health-check', (req, res) => res.send('OK'))
   .use('/auth', require('./auth'))
-  .use('/user', require('./user'))
+  .use('/users', require('./user'))
   .use('/games', require('./games'))
