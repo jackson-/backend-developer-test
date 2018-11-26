8aacfa1fa7c5452afbe573650f091e3a630accd9
jackson- <jackson.t.devin@gmail.com>
2018-11-26 18:19:11 -0500

    used updates prop on body to update user, this can be used for age, location and other property udpates. you can add list players by interest and sort palyers by distance

--- routes/user.js
+++ routes/user.js
@@ -1,35 +1,76 @@
-const router = require('express').Router();
-const authService = require('../Services/AuthService');
-const {User} = require('../Model');
+const router = require("express").Router();
+const authService = require("../Services/AuthService");
+const { User } = require("../Model");
+const mongoose = require("mongoose");
 
 module.exports = router
-    .post('/update', [authService.checkTokenMW, authService.verifyToken], async (req, res) => {
-        const user = await User.findOneAndUpdate({_id: req.body.user._id}, {...req.body.user})
-        if(user){
-            res.sendStatus(200);
-        } else {
-            res.sendStatus(400)
-        }
-    })
-    .post('/listPlayers', [authService.checkTokenMW, authService.verifyToken], async (req, res) => {
-        try{
-            const {user} = req.body;
-            const players = await User.find({preferences: {$in: user.preferences}, _id: {$ne: user._id}}).populate('preferences')
-            return res.status(200).send(players)
-        } catch(e) {
-            return res.status(400).send(e.message)
-        }
-    })
-    .post('/addPreference', [authService.checkTokenMW, authService.verifyToken], async (req, res) => {
-        try{
-            await User.updateOne(
-                { _id: req.body.userId},
-                { $addToSet: {preferences: [ req.body.gameId ] } }
-            )
-            const user = await User.findOne({ _id: req.body.userId}).populate("preferences")
-            res.status(200).send(user);
-        } catch(e) {
-            res.status(400).send(e.message)
-        }
-    })
-
+  .post(
+    "/update",
+    [authService.checkTokenMW, authService.verifyToken],
+    async (req, res) => {
+      const user = await User.findOneAndUpdate(
+        { _id: req.body.user._id },
+        { ...req.body.updates }
+      );
+      if (user) {
+        res.sendStatus(200);
+      } else {
+        res.sendStatus(400);
+      }
+    }
+  )
+  .get(
+    "/listPlayers",
+    [authService.checkTokenMW, authService.verifyToken],
+    async (req, res) => {
+      try {
+        const { user } = req.body;
+        const user_id = mongoose.Types.ObjectId(user._id);
+        const pref_ids = user.preferences.map((p) => mongoose.Types.ObjectId(p))
+        const players = await User.aggregate([
+            {
+                $geoNear: {
+                    near: { 
+                    type: "Point",
+                    coordinates: [ 75.7873, 26.9124]
+                    },
+                    distanceField: "dist.calculated",
+                    maxDistance: 2,
+                    spherical: true
+                }
+            },
+            {
+                $match: {
+                    _id: { $ne: user_id },
+                },
+            },
+            {
+                $match: {
+                    preferences: { $in: pref_ids },
+                },
+            },
+        ]);
+        return res.status(200).send(players);
+      } catch (e) {
+        return res.status(400).send(e.message);
+      }
+    }
+  )
+  .post(
+    "/addPreference",
+    [authService.checkTokenMW, authService.verifyToken],
+    async (req, res) => {
+      try {
+        await User.updateOne(
+          { _id: req.body.userId },
+          { $addToSet: { preferences: [req.body.gameId] } }
+        );
+        const user = await User.findOne({ _id: req.body.userId }).populate(
+          "preferences"
+        );
+        res.status(200).send(user);
+      } catch (e) {
+        res.status(400).send(e.message);
+      }
+    }
+  );
