f7d9205e134590fc64e225363f9ac8bb84979545
jackson- <jackson.t.devin@gmail.com>
2018-11-26 15:34:57 -0500

    removed game preference model

--- Model/index.js
+++ Model/index.js
@@ -1,6 +1,5 @@
 module.exports = {
     Game: require('./Game'),
-    GamePreference: require('./GamePreference'),
     Match: require('./Match'),
     MatchPlayer: require('./MatchPlayer'),
     User: require('./User'),
